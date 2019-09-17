84c21050ef3da9e33e27ddf8d65dce4946f79bea
John Leonard <jellyedwards@hotmail.com>
2019-09-17 17:37:50 +0100

    call git from icon in menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { runGitCommandInTerminal } from './terminal';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.assumeUnchanged', (file) => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
+		vscode.window.showInformationMessage('Hello VS Code! ' + file.resourceUri.fsPath);
+		runGitCommandInTerminal(context, 'status', '', '', true);
 	});
 
 	context.subscriptions.push(disposable);
