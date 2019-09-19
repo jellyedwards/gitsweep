4490bbe23549064473b87039bbf75cf4b83e089f
John Leonard <jellyedwards@hotmail.com>
2019-09-19 15:29:20 +0100

    refactor and rename

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { GauProvider, AssumedUnchangedFile, IgnoreEnum } from './gauProvider';
-import * as cp from 'child_process';
-import * as fs from 'fs';
-import * as path from 'path';
+import { GitSweep } from './gitSweep';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,100 +9,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gitassumeunchanged" is now active!');
+	console.log('Congratulations, your extension "gitSweep" is now active!');
 
-	// get the git root dir
-	const cwd = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : './';
-	const gitRoot = cp.execSync('git rev-parse --show-toplevel', { cwd }).toString().replace(/\n$/, '');
-	const excludeFileLocation = path.join(gitRoot, '.git', 'info', 'exclude');
-
-	const gauProvider = new GauProvider(gitRoot);
-	vscode.window.registerTreeDataProvider('gauAssumedUnchanged', gauProvider);
+	const gitSweep = new GitSweep();
+	vscode.window.registerTreeDataProvider('gitSweep', gitSweep);
 
 	// TODOJL proper error handling, none of this return true false that isn't used
-	// TODOJL refactor into own class
-	const fileInRepo = (filename: string) => {
-		try {
-			cp.execSync(`git ls-files --error-unmatch ${filename}`, { cwd: gitRoot });
-			return true;
-		} catch {
-			return false;
-		}
-	};
 
-	const excludeFile = (filename: string) => {
-		try {
-			const prefix = fs.readFileSync(excludeFileLocation).toString().endsWith('\n') ? '' : '\n';
-			fs.appendFileSync(excludeFileLocation, prefix + filename + '\n');
-			return true;
-		} catch {
-			return false;
-		}
-	};
 
-	const includeFile = (filename: string) => {
-		try {
-			const newExclude = fs.readFileSync(excludeFileLocation)
-				.toString()
-				.replace(filename+'\n', '');
-
-			fs.writeFileSync(excludeFileLocation, newExclude);
-			
-			return true;
-		} catch(err) {
-			return false;
-		}
-	};
-
-	// TODOJL: skip worktree might be better: http://blog.stephan-partzsch.de/how-to-ignore-changes-in-tracked-files-with-git/
-	const updateIndex = (filename: String, arg: string, msg: string) => {
-		try {
-			cp.execSync(`git update-index ${arg} ${filename}`, { cwd: gitRoot });
-			vscode.window.showInformationMessage(`${msg} ${filename}`);
-		} catch (err) {
-			vscode.window.showErrorMessage(err.message);
-		}
-	};
-
-	const skip = (f: String) => updateIndex(f, '--skip-worktree', 'Skip Worktree');
-	const noSkip = (f: String) => updateIndex(f, '--no-skip-worktree', 'No Skip Worktree');
-	// const assume = (f: String) => updateIndex(f, '--assume-unchanged', 'Assume Unchanged');
-	const noAssume = (f: String) => updateIndex(f, '--no-assume-unchanged', 'No Assume Unchanged');
-
-	// TODOJL: if the file isn't in the repo already add it to the exclude dir
-	// this project does that already: https://github.com/BouKiCHi/git-exclude/blob/4837b0bbca08c2100457c3fe0c80d9a249dcc2ca/src/extension.ts
-	// but it assumes .git is in the workspace root
-	// git rev-parse --show-toplevel
-	// gives the actual git repo location
-	// git ls-files --others
-	// shows them
-	context.subscriptions.push(vscode.commands.registerCommand('extension.assumeUnchanged', (item) => {
-		// check if the file is in the repo already
-		const file = item.resourceUri.fsPath;
-		if (fileInRepo(file)) {
-			skip(file);
-		} else {
-			excludeFile(item.resourceUri.path.replace(gitRoot, '').replace('//', '/'));
-		}
-
-		gauProvider.refresh();
-		vscode.commands.executeCommand('git.refresh');
+	context.subscriptions.push(vscode.commands.registerCommand('gitSweep.sweep', (item) => {
+		gitSweep.sweepFile(item.resourceUri.fsPath);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.noAssumeUnchanged', (item) => {
-		const file = item.path;
-		if (fileInRepo(file)) {
-			if (item.type === IgnoreEnum.AssumeUnchanged) {
-				noAssume(file);
-			} else {
-				noSkip(file);
-			}
-		} else {
-			includeFile(item.filename);
-		}	
-
-		gauProvider.refresh();
-		vscode.commands.executeCommand('git.refresh');
+	context.subscriptions.push(vscode.commands.registerCommand('gitSweep.unsweep', (item) => {
+		gitSweep.unsweepFile(item.path, item.type);
 	}));
 }
 
---
+++ src/gitSweep.ts
@@ -0,0 +1,191 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as cp from 'child_process';
+
+const IgnoreEnum = Object.freeze({
+	AssumeUnchanged: 'Assume Unchanged',
+	SkipWorktree: 'Skip Worktree',
+	Excluded: 'Excluded'
+});
+
+export class GitSweep implements vscode.TreeDataProvider<AssumedUnchangedFile> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<AssumedUnchangedFile | undefined> = new vscode.EventEmitter<AssumedUnchangedFile | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<AssumedUnchangedFile | undefined> = this._onDidChangeTreeData.event;
+
+	private pathToExclude: string;
+	private gitRoot: string;
+
+	constructor() {
+		const cwd = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : './';
+		this.gitRoot = cp.execSync('git rev-parse --show-toplevel', { cwd }).toString().replace(/\n$/, '');
+		this.pathToExclude = path.join(this.gitRoot, '.git', 'info', 'exclude');
+	}
+
+	sweepFile(path: string) {
+		if (this.fileInRepo(path)) {
+			this.skip(path);
+		} else {
+			//excludeFile(item.resourceUri.path.replace(gitRoot, '').replace('//', '/'));
+			this.excludeFile(path);
+		}
+
+		this.refresh();
+		vscode.commands.executeCommand('git.refresh');
+	}
+
+	unsweepFile(path: string, type: string) {
+		if (this.fileInRepo(path)) {
+			if (type === IgnoreEnum.AssumeUnchanged) {
+				this.dontAssume(path);
+			} else {
+				this.dontSkip(path);
+			}
+		} else {
+			this.includeFile(path);
+		}	
+
+		this.refresh();
+		vscode.commands.executeCommand('git.refresh');
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: AssumedUnchangedFile): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: AssumedUnchangedFile): Thenable<AssumedUnchangedFile[]> {
+		// get the files that are skipped or assume-unchanged
+		const gitls = cp.execSync('git ls-files -v', {cwd: this.gitRoot});
+		const assumedUnchangedFiles = gitls.toString().split(/\n/)
+			.filter(a => /^[a-zS]/.test(a))
+			.map(line => {
+				const parts = line.split(" ");
+				let type = IgnoreEnum.SkipWorktree;
+				if (parts[0] === parts[0].toLowerCase()) {
+					type = IgnoreEnum.AssumeUnchanged;
+				}
+
+				return {
+					type,
+					path: parts[1]
+				};
+			});
+
+		// get the files that are in the exclude file
+		const excludedFiles = fs.readFileSync(this.pathToExclude)
+			.toString().split(/\n/)
+			.filter(e => /^(?!\s*#).+/.test(e))
+			.map(line => ({ type: IgnoreEnum.Excluded, path: line}));
+
+		return Promise.resolve(assumedUnchangedFiles.concat(excludedFiles)
+			.map(file => new AssumedUnchangedFile(file.type, file.path, this.gitRoot)));
+	}
+
+	// private pathExists(p: string): boolean {
+	// 	try {
+	// 		fs.accessSync(p);
+	// 	} catch (err) {
+	// 		return false;
+	// 	}
+
+	// 	return true;
+	// }
+
+	private fileInRepo = (filename: string) => {
+		try {
+			cp.execSync(`git ls-files --error-unmatch ${filename}`, { cwd: this.gitRoot });
+			return true;
+		} catch {
+			return false;
+		}
+	}
+
+	private excludeFile = (filename: string) => {
+		try {
+			filename = this.shortenPath(filename);
+			const prefix = fs.readFileSync(this.pathToExclude).toString().endsWith('\n') ? '' : '\n';
+
+			fs.appendFileSync(this.pathToExclude, prefix + filename + '\n');
+			
+			return true;
+		} catch (err) {
+			console.error("couldn't exclude file ", err);
+
+			return false;
+		}
+	}
+
+	private forwardSlashes(s: string) {
+		return s.replace(/\\/g, '/');
+	}
+
+	private shortenPath(p: string) {
+		return (this.forwardSlashes(p).replace(this.forwardSlashes(this.gitRoot), ''));
+	}
+
+	private includeFile = (filename: string) => {
+		try {
+			filename = this.shortenPath(filename);
+			const newExclude = fs.readFileSync(this.pathToExclude)
+				.toString()
+				.replace(filename+'\n', '')
+				.replace(filename, ''); // or if there wasn't a newline after it
+
+			fs.writeFileSync(this.pathToExclude, newExclude);
+			
+			return true;
+		} catch(err) {
+			console.error("couldn't include file ", err);
+			return false;
+		}
+	}
+
+	private updateIndex = (filename: String, arg: string, msg: string) => {
+		try {
+			cp.execSync(`git update-index ${arg} ${filename}`, { cwd: this.gitRoot });
+			vscode.window.showInformationMessage(`${msg} ${filename}`);
+		} catch (err) {
+			vscode.window.showErrorMessage(err.message);
+		}
+	}
+
+	private skip = (f: String) => this.updateIndex(f, '--skip-worktree', 'Skip Worktree');
+	private dontSkip = (f: String) => this.updateIndex(f, '--no-skip-worktree', 'No Skip Worktree');
+	// private assume = (f: String) => updateIndex(f, '--assume-unchanged', 'Assume Unchanged');
+	private dontAssume = (f: String) => this.updateIndex(f, '--no-assume-unchanged', 'No Assume Unchanged');
+
+}
+
+class AssumedUnchangedFile extends vscode.TreeItem {
+	constructor(
+		public readonly type: string,
+		public readonly filename: string,
+		public readonly gitRoot: string,
+		public readonly command?: vscode.Command	// command exec'd when selected
+	) {
+		super(vscode.Uri.file(path.join(gitRoot, filename)),
+			vscode.TreeItemCollapsibleState.None);
+	}
+
+	get path(): string {
+		return path.join(this.gitRoot, this.filename);
+	}
+
+	get tooltip(): string {
+		return `${this.filename}·${this.type}`;
+	}
+
+	get description(): string {
+		return this.filename;
+	}
+
+	// iconPath = {
+	// 	light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+	// 	dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	// };
+}
